ff7d83bf47a9c8cb35f88ed175d190427cd400fa
yudapratama-del <57089058+yudapratama-del@users.noreply.github.com>
2019-10-30 09:11:25 +0800

    Create arrowfunct.js

---
+++ arrowfunct.js
@@ -0,0 +1,8 @@
+
+hello = () => {
+  document.getElementById("demo").innerHTML += this;
+}
+
+window.addEventListener("load", hello);
+
+document.getElementById("btn").addEventListener("click", hello);
